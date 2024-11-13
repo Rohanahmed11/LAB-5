4fedd0e0c02958ad482abdf2bcf9171ac18ce3df
Rohanahmed11 <roah4500@gmail.com>
2024-11-14 00:40:27 +0500

    Add files via upload

---
+++ SC & D (LAB # 05)/AlphabetThread.java
@@ -0,0 +1,33 @@
+package Task1;
+import java.util.Random;
+
+class AlphabetThread implements Runnable {
+
+    @Override
+    public void run() {
+        Random random = new Random();
+        
+
+        for (int i = 0; i < 26; i++) {
+            char randomChar = (char) ('A' + random.nextInt(26));
+            
+            System.out.print(randomChar + " ");
+            
+            try {
+                int randomDelay = 100 + random.nextInt(400);
+                Thread.sleep(randomDelay); 
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+            
+            if (i == 20) { 
+                System.out.println("\nStopping the thread now...");
+                Thread.currentThread().stop();  
+                break;
+            }
+        }
+        
+        // Ensure the last character is printed in the same line
+        System.out.println();
+    }
+}
