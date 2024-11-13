4e75bc9e18184ffa2dd96b471c6391320623d768
Rohanahmed11 <roah4500@gmail.com>
2024-11-13 19:39:38 +0000

    Delete Task1 directory

--- Task1/AlphabetPrinter.java
+++
@@ -1,9 +0,0 @@
-package Task1;
-public class AlphabetPrinter {
-
-    public static void main(String[] args) {
-        AlphabetThread alphabetThread = new AlphabetThread();
-        Thread thread = new Thread(alphabetThread);
-        thread.start();
-    }
-}
